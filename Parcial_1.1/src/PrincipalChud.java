939524990630862a81f9995ce76b3e38f7ccc9d0
Yazsi96 <31865577+Yazsi96@users.noreply.github.com>
2017-09-12 00:14:17 -0600

    Add files via upload

---
+++ Parcial_1.1/src/PrincipalChud.java
@@ -0,0 +1,10 @@
+
+public class PrincipalChud {
+	public static void PrincipalChud (String[] args)
+	{
+		AlgortimoChud ACh=new AlgortimoChud();
+		System.out.println(ACh.Chudnovsky(5));
+		
+	}
+
+}
